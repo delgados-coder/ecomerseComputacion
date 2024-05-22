96144d7f7f83a99aaf557bac57dc780049e1797f
delgados-coder <delgados.coder@gmail.com>
2024-05-22 03:19:16 -0300

    se agrego una nueva estructura de archivos. se agrego una DESC.md de las tareas prncipales de cada hoja de estilo, se estructuro los archivos php para modulizar el proyecto

---
+++ app/layout/_head.php
@@ -0,0 +1,9 @@
+<?php
+echo"<h1>**_head.php</h1>";
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TITULO</title>
+</head>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo"<h1>index.php</h1>";
+require_once "./app/html.php";
+?>
